a754f4f36c22123a0c74f949a37d59331b3d684e
V1tale <elativ.c@gmail.com>
2022-10-28 17:11:34 +0200

    pushare emails in array

--- js/script.js
+++ js/script.js
@@ -3,19 +3,27 @@ createApp({
     data(){
         return {
             emailArray: [],
+            email: "",
         }
+    },
+    created() {
+        this.randomEmailGenerator();
     },
     methods: {
         randomEmailGenerator() {
+            for ( let i = 0; i < 10; i++) {
+
             axios
             .get(`https://flynn.boolean.careers/exercises/api/random/mail`)
-            .then ((fromServer) => {
-                console.log(fromServer);
-
-            })
-        }
+            .then ((fromServer) =>  {
+             this.email = fromServer.data.response;
+             console.log(this.email);
+            });
+            this.emailArray.push(this.email);
+        };
     },
 
 
 
-}).mount("#app");
+
+}}).mount("#app");
